aba0a0cbb70918e54bf8bd1fd75372b8495389ca
Alon Dahari <climbinghobo@gmail.com>
2015-02-17 16:12:06 -0700

    add entry view

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', indexController.index);
+app.get('/entry-form', indexController.entry);
 
 var server = app.listen(6843, function() {
 	console.log('Express server listening on port ' + server.address().port);
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 var indexController = {
 	index: function(req, res) {
 		res.render('index');
-	}
+	},
+	entry: function(req, res) {
+		res.render('entry');
+	},
+
 };
 
 module.exports = indexController;
